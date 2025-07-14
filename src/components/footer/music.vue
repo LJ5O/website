278bfb3866b56c2068b12f95310cfd512f364e65
LJ5O <75009579+LJ5O@users.noreply.github.com>
2025-07-14 15:04:35 +0200

    New songs

--- src/components/footer/music.vue
+++ src/components/footer/music.vue
@@ -11,12 +11,19 @@
         "https://youtu.be/KPTALvwccME",
         "https://www.youtube.com/watch?v=I-FqKYGK5_M",
         "https://www.youtube.com/watch?v=YUUFtUjiblk",
-        "https://www.youtube.com/watch?v=wEwnEeq23SY",
+        //"https://www.youtube.com/watch?v=wEwnEeq23SY",
         "https://youtu.be/s28KGdOVaZ0",
         "https://youtu.be/esofirhSD-o",
         "https://youtu.be/5lrseDtxX_E",
         "https://www.youtube.com/watch?v=dQw4w9WgXcQ", // Never gonna give you up :]
-        "https://youtu.be/5-sfG8BV8wU"
+        "https://youtu.be/5-sfG8BV8wU",
+        "https://youtu.be/rarpN863WlE",
+        "https://youtu.be/VA7rto4XuV0",
+        "https://youtu.be/d1d6vPTwoKU",
+        "https://www.youtube.com/watch?v=ibvGROS0LwY",
+        "https://www.youtube.com/playlist?list=PL8DD873BF6A3E56E5",
+        "https://www.youtube.com/watch?v=8yISocSYXvc"
+
     ];
 
     const open_music = ()=>{
